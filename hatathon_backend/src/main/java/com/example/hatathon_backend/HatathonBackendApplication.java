46308364212887b5bbe8be362f809b11410d7d2e
ivanmaister7 <ivanmaister7@gmail.com>
2023-07-27 23:56:57 +0300

    init commit

---
+++ hatathon_backend/src/main/java/com/example/hatathon_backend/HatathonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.hatathon_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatathonBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HatathonBackendApplication.class, args);
+    }
+
+}
